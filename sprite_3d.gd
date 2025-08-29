eb5d2f1980ab42a32f11d520f9f378faa4fb69cc
Bright Li <100brightli@gmail.com>
2025-08-29 11:47:40 -0700

    add body to the player

--- sprite_3d.gd
+++ sprite_3d.gd
@@ -1,16 +1,19 @@
 extends Sprite3D
-
-@export var speed: float = 20.0
-
-func _process(delta):
-	var input = 0.0
-	var inputy= 0.0
-	if Input.is_action_pressed("move_left"):
-		input -= 1.0
-	if Input.is_action_pressed("move_right"):
-		input += 1.0
-	if Input.is_action_just_pressed("jump"):
-		inputy += 5.0
-
-	position.x += input * speed * delta
-	position.y += inputy * speed * delta
+#
+#@export var speed: float = 20.0
+#func _process(delta):
+	#var input = 0.0
+	#var inputy= 0.0
+	#if Input.is_action_pressed("move_left"):
+		#input -= 1.0
+	#if Input.is_action_pressed("move_right"):
+		#input += 1.0
+	#if Input.is_action_just_pressed("jump"):
+		#inputy += 10.0
+	#else:
+		#inputy += -0.5
+#
+	#position.x += input * speed * delta
+	#position.y += inputy * speed * delta
+	#if position.y < 2:
+		#position.y = 2   
