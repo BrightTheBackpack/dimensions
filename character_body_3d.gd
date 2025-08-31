e704fb97a019da2a12a9a8e8eca7eee9d7ddbbe4
BrightTheBackpack <100brightli@gmail.com>
2025-08-30 20:32:57 -0700

    added wall, fixed some physics, make materials better(still need to be changed), attempted to start making instatiable platform/wall objects

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,14 +1,22 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-
+const SPEED = 10.0
+@export var JUMP_VELOCITY = 13.0
+@export var GRAVITY = -15.0
+@export var GRAVITY_DOWN = -75.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		print_debug(velocity)
+		if(velocity.y > 0.9*JUMP_VELOCITY):
+			
+			velocity += Vector3(0.0, GRAVITY, 0.0) * delta
+			
+		elif(velocity.y >0):
+			velocity += Vector3(0.0, GRAVITY_DOWN/1.3 , 0.0) * delta
+		else:
+			velocity += Vector3(0.0, GRAVITY_DOWN, 0.0) * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
---
+++ platform_gen.gd
@@ -0,0 +1,33 @@
+tool
+extends Node3D
+@onready var collision_shape: CollisionShape3D = $Node3D/CollisionShape3D
+@onready var mesh_instance: MeshInstance3D = $Node3D/MeshInstance3D
+
+
+#@export var x_size: float = 5.0 : set = _set_size
+#@export var y_size: float = 5.0 : set = _set_size
+#@export var z_size: float = 5.0 : set = _set_size
+@export var x_size: float = 5.0 
+@export var y_size: float = 5.0 
+@export var z_size: float = 5.0 
+
+#func _ready():
+	#_update_size()
+#
+#func _set_size(value):
+	## this setter runs whenever one of the exported vars changes
+	#_update_size()
+
+
+
+func _process(delta):
+	# Mesh scaling (assuming you’re using a primitive mesh, like a BoxMesh)
+	print(mesh_instance)
+	var box = BoxMesh.new()
+	box.size = Vector3(x_size, y_size, z_size)
+	mesh_instance.mesh = box
+	
+
+	# Collision scaling (assuming you’re using a BoxShape3D)
+	if collision_shape.shape is BoxShape3D:
+		collision_shape.shape.extents = Vector3(x_size, y_size, z_size) * 0.5
